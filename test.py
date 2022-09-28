449b739b9df432c3dff97840a020233d95442552
atglabchoi <hs.choi@atglab.co.kr>
2022-09-28 16:47:12 +0900

    second

--- test.py
+++ test.py
@@ -33,4 +33,4 @@ def index():
 if __name__ == '__main__':
     #app.run()
     #호스트등을 직접 지정하고 싶을 경우.
-    app.run(host="0.0.0.0", port="8080", debug=True)
+    app.run(host="0.0.0.0", port="5000", debug=True)
